4a0e46b99e34e087fe58d8eacf52930caa57a562
williajess7 <jessenw@outlook.com>
2019-10-11 22:25:16 +1300

    added bar graph for room counters

--- mqtt-client/src/app/home/home.page.ts
+++ mqtt-client/src/app/home/home.page.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 import { NavController } from '@ionic/angular';
 
 import { MQTTService} from 'ionic-mqtt';
+import { Chart } from 'chart.js';
 
 // @ts-ignore
 @Component({
@@ -29,7 +30,54 @@ export class HomePage {
 
   private TOPIC: string[] = ['swen325/a3'];
 
+  @ViewChild("barCanvas", {static: false}) barCanvas: ElementRef;
+  private barChart: Chart;
+
+
   constructor(public navCtrl: NavController, private mqttService: MQTTService) {}
+
+  ionViewDidEnter() {
+    this.barChart = new Chart(this.barCanvas.nativeElement, {
+      type: "bar",
+      data: {
+        labels: ["Living", "Kitchen", "Dining", "Toilet", "Bedroom", "Orange"],
+        datasets: [
+          {
+            label: "# of Votes",
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+              "rgba(255, 99, 132, 0.2)",
+              "rgba(54, 162, 235, 0.2)",
+              "rgba(255, 206, 86, 0.2)",
+              "rgba(75, 192, 192, 0.2)",
+              "rgba(153, 102, 255, 0.2)",
+              "rgba(255, 159, 64, 0.2)"
+            ],
+            borderColor: [
+              "rgba(255,99,132,1)",
+              "rgba(54, 162, 235, 1)",
+              "rgba(255, 206, 86, 1)",
+              "rgba(75, 192, 192, 1)",
+              "rgba(153, 102, 255, 1)",
+              "rgba(255, 159, 64, 1)"
+            ],
+            borderWidth: 1
+          }
+        ]
+      },
+      options: {
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true
+              }
+            }
+          ]
+        }
+      }
+    });
+  }
 
   public _onConnectionLost = (responseObject) => {
     if (responseObject.errorCode !== 0) {
